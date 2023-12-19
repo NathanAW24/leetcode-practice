4617e2bff2aad63b5eaf665fa1e48f2957320f58
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-12-20 00:26:37 +0700

    problem done with explanation and dictionary solution, storing thisIterationSum in dict, to be referred in next iteration, as prevIterationSum

--- src/Problem560SubarraySumEqualsK/subarray_sum_eq_k.py
+++ src/Problem560SubarraySumEqualsK/subarray_sum_eq_k.py
@@ -3,17 +3,51 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        noOfPrefixSumHash = {}  # {prefixSum: {how many times prefixSum appears}}
+        # {sumValue: {how many times sumValue appears}}
+        # {0: 1} case, is edge case for when sumFrom(nums[0]...nums[i]) == k directly
+        # it represents the empty subarray behind the first element
+        noOfSumValueHash = {0: 1}
         count = 0
-        subSum = 0
+        thisIterationSum = 0
 
         for i in range(len(nums)):
-            subSum = 0
-            for j in range(i, len(nums)):
-                subSum = subSum + nums[j]
-                if subSum == k:
-                    count += 1
+            # sum of array from nums[0]...nums[i] inclusive
+            thisIterationSum = thisIterationSum + nums[i]
 
+            # COUNT ADDITION STEP
+            # want to find ==> subarraySum == k
+            # sumFrom(nums[j+1]...nums[i]) = k, abaikan spesifik index-nya
+            # sumFrom(nums[0]...nums[i]) - sumFrom(nums[0]..nums[j]) = k
+            # sumFrom(thisIteration) - sumFrom(previousIteration) = k
+            # sumFrom(previousIteration) = sumFrom(thisIteration) - k
+            prevIterationSum = thisIterationSum - k
+            # check if sumFrom(previousIteration) is recorded alr
+            # the sum value from previousIteration subarrays can appear more than 1 time
+            if prevIterationSum in noOfSumValueHash:
+                count = count + noOfSumValueHash[prevIterationSum]
+
+            # PUT IN DICTIONARY STEP
+            # here thisIterationSum is sumFrom(nums[0]...nums[i]) = sumFrom(thisIteration)
+            # then this will be recorded
+            # next iteration(s) this recorded value appear as sumFrom(previousIteration) = sumForm(nums[0]...nums[i])
+            #
+            # e.g.
+            # iteration i = 2, k = 6
+            # found thisIterationSum = 4
+            # ... stuff with prevIterationSum (neglect this for now), assuming prevIterationSum not in dict
+            # put/update thisIterationSum in dict { ..., 4: *someInt ,...}
+            # iteration i = 3
+            # found thisIterationSum = 10
+            # prevIterationSum = 4, which is the value of the previous thisIterationSum
+            # goes to the {count += *someInt} block
+            if thisIterationSum not in noOfSumValueHash:
+                noOfSumValueHash[thisIterationSum] = 1
+            else:  # thisIterationSum has appeared previously as prevIterationSum, hence increment only...
+                # basically saying to the next iteration(s) there is more than one iterationSum arrays, that results in same value
+                # the next iteration(s) that satisfies conditions, can work with all of those iterationSum arrays
+                noOfSumValueHash[thisIterationSum] = noOfSumValueHash[thisIterationSum] + 1
+                # more accurate formula below, but values are same (prevIterationSum == thisIterationSum) so ...
+                # noOfSumValueHash[prevIterationSum] = noOfSumValueHash[thisIterationSum] + 1
         return count
 
 
