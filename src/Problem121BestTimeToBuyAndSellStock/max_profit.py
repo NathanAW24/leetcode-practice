0e8bc152f1f3d4cb72ff2a7246f56f3d844de91e
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-09-16 14:56:10 +0800

    neetcode solution

--- src/Problem121BestTimeToBuyAndSellStock/max_profit.py
+++ src/Problem121BestTimeToBuyAndSellStock/max_profit.py
@@ -3,15 +3,19 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        max_profit = 0
+        right = 1  # buy
+        left = 0  # sell
 
-        for right in range(1, len(prices)):
-            left = 0
-            while left < right:
-                profit = max(profit, prices[right] - prices[left])
-                print(left, right, profit)
-                left += 1
-        return profit
+        while right < len(prices):
+            if prices[right] - prices[left] > 0:
+                max_profit = max(max_profit, prices[right] - prices[left])
+            else:  # sell is smaller than buy, malah rugi, means there's a better value in the future to buy from, which will confirm yield to greater profit than if we kept the left value to current
+                left = right
+
+            right += 1
+
+        return max_profit
 
 
 prices = [7, 1, 5, 3, 6, 4]
