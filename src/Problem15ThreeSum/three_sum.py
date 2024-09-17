43e11e21208397e3ccf0307adb032a5856c627ef
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-09-17 14:16:00 +0800

    remove comments and revert to neetcode solution checking from left

--- src/Problem15ThreeSum/three_sum.py
+++ src/Problem15ThreeSum/three_sum.py
@@ -21,10 +21,9 @@ class Solution:
                     left += 1
                 else:
                     res.append([num, nums[left], nums[right]])
-
-                    right -= 1
-                    while nums[right] == nums[right+1] and left < right:
-                        right -= 1
+                    left += 1
+                    while nums[left] == nums[left-1] and left < right:
+                        left += 1
 
         return res
 
