1e2d17bccc9ae3c09e337271f934604fc0916573
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-09-17 14:12:06 +0800

    code with comments

--- src/Problem15ThreeSum/three_sum.py
+++ src/Problem15ThreeSum/three_sum.py
@@ -16,17 +16,24 @@ class Solution:
                 # we don't need to do the same thing again here (we don't want duplicates)
                 continue
 
+            # the rest is two pointer solution for sorted array for index [i+1, ..., n-1] inclusive
+            # basically consider the values for the rest of the array after `i`
+            # only need to consider after `i` as we don't want repeated indices or values selected againf
             left, right = i + 1, len(nums)-1
             while left < right:
                 three_sum = num + nums[left] + nums[right]
-                if three_sum > 0:
-                    right -= 1
+                if three_sum > 0:  # 0 is `target`, can be changed to `target` given the right problem
+                    right -= 1  # we need to reduce the sum, move `right` pointer left to reduce value of `three_sum`
                 elif three_sum < 0:
-                    left += 1
-                else:
+                    left += 1  # we need to increase the sum, move 'left' pointer right to increase value of `three_sum`
+                else:  # exactly eq to `target` `0`
                     res.append([num, nums[left], nums[right]])
-                    left += 1
+
+                    left += 1  # move `left` pointer right, to avoid finding duplicates
                     while nums[left] == nums[left-1] and left < right:
+                        # similar to check in the beginning
+                        # to consider the case for [..., -3, -3,...], duplicate values being
+                        #                                     ^
                         left += 1
 
         return res
