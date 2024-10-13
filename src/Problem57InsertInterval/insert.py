1f81e7b5321129a2fdee3969ba9e633348e8b569
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-13 21:17:10 +0800

    code works after seeing neetcode solution

--- src/Problem57InsertInterval/insert.py
+++ src/Problem57InsertInterval/insert.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = [newInterval]
+        result = []
 
         for i, interval in enumerate(intervals):
             # check if this interval merges with newInterval
@@ -11,41 +11,50 @@ class Solution:
 
             new_l, new_r = newInterval
 
-            if new_l <= anchor_l <= anchor_r <= new_r:
+            if new_r < anchor_l:  # newInterval is at the left of some interval at i
+                print(
+                    f"newInterval {newInterval} at the left of some interval {i} {interval}")
+                result.append(newInterval)
+                return result + intervals[i:]
+
+            elif new_l > anchor_r:
+                print(
+                    f"newInterval {newInterval} is at the right of interval {i} {interval}, safely append the interval")
+
+                result.append(interval)
+
+            elif new_l <= anchor_l <= anchor_r <= new_r:
                 print(
                     f"Yellow merge interval {interval} with newInterval {newInterval}")
                 newInterval = [new_l, new_r]
-
-                result.append(newInterval)
 
             elif anchor_l <= new_l <= new_r <= anchor_r:
                 print(
                     f"Pink merge interval {interval} with newInterval {newInterval}")
                 newInterval = [anchor_l, anchor_r]
 
-                result.append(newInterval)
-
             elif new_l <= anchor_l <= new_r:
                 print(
                     f"Red merge interval {interval} with newInterval {newInterval}")
                 newInterval = [new_l, anchor_r]
-
-                result.append(newInterval)
 
             elif new_l <= anchor_r <= new_r:
                 print(
                     f"Blue merge interval {interval} with newInterval {newInterval}")
                 newInterval = [anchor_l, new_r]
 
-                result.append(newInterval)
+            # elif new_r < anchor_l:  # newInterval is at the left of some interval at i
+            #     print(
+            #         f"newInterval {newInterval} at the left of some interval {i} {interval}")
+            #     result.append(newInterval)
 
-            else:  # no intervals at all
-                print(
-                    f"NO merge, interval {interval} with newInterval {newInterval}")
+            # elif new_l > anchor_r:
+            #     print(
+            #         f"newInterval {newInterval} is beyond interval {i} {interval}, safely append the interval")
 
-                result.append(interval)
+            #     result.append(interval)
 
-            print(f'the newInterval is {newInterval}')
+            print(f'the newInterval is {newInterval}, result is {result}')
 
         result.append(newInterval)
 
