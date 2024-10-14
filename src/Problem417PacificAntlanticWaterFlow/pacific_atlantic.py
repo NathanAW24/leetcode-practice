a2738f55671c563b3ca13f5bd54145a84dd663d6
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-14 14:50:40 +0800

    boilerplate

---
+++ src/Problem417PacificAntlanticWaterFlow/pacific_atlantic.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        return
+
+
+heights = [[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [
+    2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]]
+print(Solution().pacificAtlantic(heights))
+
+heights = [[1]]
+print(Solution().pacificAtlantic(heights))
